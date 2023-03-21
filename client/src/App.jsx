4ba69ae12e7bcfd4f3e3f65130b4c3886a76717a
sangeetshrivastava <sangeetshrivastava60@gmail.com>
2023-03-21 22:40:56 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./global/global.scss";
+import "./global/global.css";
+import Header from "./components/common/header/Header";
+import Footer from "./components/common/Footer";
+
 
 function App() {
   return (
     <div>
+      <Header/>
       <h1>Welcome to Code Crew</h1>
+      <Footer/>
       <p>Hi, How can I help you??</p>
       <p>hello User</p>
     </div>
---
+++ client/src/components/common/header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link, Text } from "@nextui-org/react";
+import { Container, Row } from '@nextui-org/react';
+import "../header/Header.scss";
+
+
+
+const Header = () => {
+    return (
+        <>
+        <Container md>
+            <Row>
+            <div className="header-main">
+                <div className="header-logo">
+                    <img src="" alt="LOGO" />
+                </div>
+                <div className="header-menu">
+                    <ul>
+                        <li><Link href="#">What We Do </Link></li>
+                        <li><Link href="#">About Us</Link></li>
+                        <li><Link href="#">Testimonials</Link></li>
+                        <li><Link href="#">Contact Us</Link></li>
+                    </ul>
+                </div>
+                
+            </div>
+            </Row>
+            </Container>
+
+
+
+        </>
+
+    )
+}
+export default Header; 
