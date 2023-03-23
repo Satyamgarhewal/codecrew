e18b68fa345da65aec6ec9ba9c3a82d474568f97
sangeetshrivastava <sangeetshrivastava60@gmail.com>
2023-03-23 08:13:06 +0530

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import "./global/global.scss";
 import "./global/global.css";
 import Header from "./components/common/header/Header";
-import Footer from "./components/common/Footer";
+import Footer from "./components/common/footer/Footer";
 import { createTheme, NextUIProvider, Text } from "@nextui-org/react"
 
 
 function App() {
   return (
-    
-      <NextUIProvider>      
-        <Header/>
-      <h1>Welcome to Code Crew</h1>
-      <Footer/>
-      <p>Hi, How can I help you??</p>
-      <p>hello User</p>
-      </NextUIProvider>
-   
+
+    <NextUIProvider>
+      <Header />
+
+
+      <Footer />
+
+    </NextUIProvider>
+
   );
 }
 
