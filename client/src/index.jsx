84643efcc84cffa44b165575617249a958ab6d41
Satyamgarhewal <garhewalsatyam996@gmail.com>
2023-03-24 00:14:56 +0530

    removed warnings

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.scss";
 
-const appElement = document.getElementById("app");
-ReactDOM.render(<App />, appElement);
+const app = createRoot(document.getElementById("app"));
+
+app.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
